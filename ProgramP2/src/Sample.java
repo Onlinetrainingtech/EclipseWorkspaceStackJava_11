1202088965707728d364c6d88156977c9032d14b
OnlineTrainer <mohamedtechdata@gmail.com>
2022-03-29 07:39:12 +0530

    Initial commit

---
+++ ProgramP2/src/Sample.java
@@ -0,0 +1,23 @@
+class Demo
+{
+	void get1()
+	{
+		System.out.println("This is get1 function..");
+	}
+	void get2()
+	{
+		System.out.println("This is get2 function..");
+	}
+}
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		Demo g1=new Demo();
+		g1.get1();
+		g1.get2();
+
+	}
+
+}
