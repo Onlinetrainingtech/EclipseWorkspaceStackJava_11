ba6d95fd94865cde1c55d81c2a78052a30bd1b97
OnlineTrainer <mohamedtechdata@gmail.com>
2022-04-29 22:00:53 +0530

    29-04-2022

--- ProjectGUI/src/AddProduct.java
+++ ProjectGUI/src/AddProduct.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
 
@@ -119,14 +120,83 @@ public class AddProduct extends JFrame {
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Search");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				try
+				{
+					String p1=textField.getText();
+					String str1="select * from product where pid='"+p1+"'";
+					Class.forName("org.h2.Driver");
+				    Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/corejava11","sa","");
+				    Statement stmt=conn.createStatement();
+				    ResultSet rs=stmt.executeQuery(str1);
+				    rs.next();
+				    String p2=rs.getString(2);
+				    String p3=rs.getString(3);
+				    String p4=rs.getString(4);
+				    textField_1.setText(p2);
+				    textField_2.setText(p3);
+				    textField_3.setText(p4);
+				    JOptionPane.showMessageDialog(btnNewButton_1,"Searching!!!");
+					
+				}
+				catch(Exception t)
+				{
+					
+				}
+			}
+		});
 		btnNewButton_1.setBounds(167, 344, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Update");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				try
+				{
+					String p1=textField.getText();
+					String p2=textField_1.getText();
+					String p3=textField_2.getText();
+					String str2="update product set pname='"+p2+"',pprice='"+p3+"' where pid='"+p1+"'";
+					Class.forName("org.h2.Driver");
+				    Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/corejava11","sa","");
+				    Statement stmt=conn.createStatement();
+				    stmt.executeUpdate(str2);
+					JOptionPane.showMessageDialog(btnNewButton_2,"Updated!!!..");
+					
+				}
+				catch(Exception t)
+				{
+					
+				}
+			}
+		});
 		btnNewButton_2.setBounds(300, 344, 89, 23);
 		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Delete");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				try
+				{
+					String p1=textField.getText();
+					String str3="delete from product where pid='"+p1+"'";
+					Class.forName("org.h2.Driver");
+				    Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/corejava11","sa","");
+				    Statement stmt=conn.createStatement();
+				    stmt.executeUpdate(str3);
+					JOptionPane.showMessageDialog(btnNewButton_3,"OhhDeleted..!!!..");
+					
+				}
+				catch(Exception t)
+				{
+					System.out.println(t);
+				}
+			}
+		});
 		btnNewButton_3.setBounds(335, 268, 89, 23);
 		contentPane.add(btnNewButton_3);
 	}
